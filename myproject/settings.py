b510ce4f15714b0f9e8143a0218c8f7a9df74628
Andrew Thomas <andr3w.l.thomas@gmail.com>
2018-04-22 21:45:40 -0400

    installed django extensions

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third party
+    'django_extensions',
     'social_django',
     # my apps
     'myauthapp',
